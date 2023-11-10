f5ee57111105d8679963b9df8cab2fdfde5b0820
fmindrayana2 <fmindrayana2@gmail.com>
2023-11-10 18:25:40 +0700

    2023-11-10

---
+++ fire250x300.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '31fdeee2fd4e7ac40899bc38733c9ce3',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//cockysnailleather.com/31fdeee2fd4e7ac40899bc38733c9ce3/invoke.js"></scr' + 'ipt>');
---
+++ fire728x90.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '1582864e0ec4b7e4ece9446cd5ebb705',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//cockysnailleather.com/1582864e0ec4b7e4ece9446cd5ebb705/invoke.js"></scr' + 'ipt>');
