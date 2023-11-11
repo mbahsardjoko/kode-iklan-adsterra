7c4ef62ac8527a9ebfabaaaad2f5f79a65beeed2
fmindrayana2 <fmindrayana2@gmail.com>
2023-11-11 19:53:13 +0700

    2023-11-11

---
+++ 160x300.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : 'ceba0e2024b8f4acfc195d87ca00e422',
+		'format' : 'iframe',
+		'height' : 300,
+		'width' : 160,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//cockysnailleather.com/ceba0e2024b8f4acfc195d87ca00e422/invoke.js"></scr' + 'ipt>');
