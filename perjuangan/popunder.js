c45800b019a235b33c404a3a446c7985a596e7a4
fmindrayana2 <fmindrayana2@gmail.com>
2024-09-25 15:12:15 +0700

    2024-09-25

--- perjuangan/popunder.js
+++ perjuangan/popunder.js
@@ -1,1 +1,1 @@
-document.write('<scr' + 'ipt type="text/javascript" src="//amusinghump.com/90/c6/b8/90c6b84f24f915450c1b7a4d77acbe01.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="//amusinghump.com/90/c6/b8/90c6b84f24f915450c1b7a4d77acbe01.js"></scr'+'ipt>')
