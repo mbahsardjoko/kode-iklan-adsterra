8c9c0ae8770f6b040f4e5e12ef7ee04500981acc
fmindrayana2 <fmindrayana2@gmail.com>
2024-09-30 13:09:58 +0700

    2024-09-30

--- maulud/native.js
+++ maulud/native.js
@@ -1,18 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Create the ad container dynamically
     var adContainer = document.createElement('div');
     adContainer.id = 'container-2fff9116256672b819523a02a8cabcd7';
-    document.body.appendChild(adContainer); // Append to body or a specific location in your HTML
+    document.body.appendChild(adContainer);
 
-    // Delay the script loading slightly to ensure everything is ready
-    setTimeout(function() {
-        // Create the script element dynamically
-        var adScript = document.createElement('script');
-        adScript.async = true;
-        adScript.setAttribute('data-cfasync', 'false');
-        adScript.src = '//amusinghump.com/2fff9116256672b819523a02a8cabcd7/invoke.js';
-
-        // Append the script to the body
-        document.body.appendChild(adScript);
-    }, 1000); // Adjust the delay if necessary
+    var adScript = document.createElement('script');
+    adScript.async = true;
+    adScript.setAttribute('data-cfasync', 'false');
+    adScript.src = '//amusinghump.com/2fff9116256672b819523a02a8cabcd7/invoke.js';
+    document.body.appendChild(adScript);
 });
