1178c8b4f8b741e2757aa9349f9555e11014016b
fmindrayana2 <fmindrayana2@gmail.com>
2024-09-30 13:52:42 +0700

    2024-09-30

--- maulud/nativeteam.js
+++ maulud/nativeteam.js
@@ -3,9 +3,13 @@
     script.async = true;
     script.setAttribute('data-cfasync', 'false');
     script.src = '//amusinghump.com/2fff9116256672b819523a02a8cabcd7/invoke.js';
-    document.head.appendChild(script);
-
-    var container = document.createElement('div');
-    container.id = 'container-2fff9116256672b819523a02a8cabcd7';
-    document.body.appendChild(container);
+    document.body.appendChild(script);
+    
+    // Pastikan elemen container sudah ada
+    var containerId = 'container-2fff9116256672b819523a02a8cabcd7';
+    if (!document.getElementById(containerId)) {
+        var container = document.createElement('div');
+        container.id = containerId;
+        document.body.appendChild(container);
+    }
 })();
