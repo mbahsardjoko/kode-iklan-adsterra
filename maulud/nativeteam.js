abddba73a7b1586edbca5302177aa7f7603f946c
fmindrayana2 <fmindrayana2@gmail.com>
2024-09-30 13:55:33 +0700

    2024-09-30

--- maulud/nativeteam.js
+++ maulud/nativeteam.js
@@ -2,7 +2,7 @@
     var script = document.createElement('script');
     script.async = true;
     script.setAttribute('data-cfasync', 'false');
-    script.src = '//amusinghump.com/2fff9116256672b819523a02a8cabcd7/invoke.js';
+    script.src = 'https://amusinghump.com/2fff9116256672b819523a02a8cabcd7/invoke.js';
     document.body.appendChild(script);
     
     // Pastikan elemen container sudah ada
