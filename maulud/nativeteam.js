dc9e5332ae794620f4cc53a1a0e7b6a5def2056f
fmindrayana2 <fmindrayana2@gmail.com>
2024-09-30 13:51:19 +0700

    2024-09-30

---
+++ maulud/nativeteam.js
@@ -0,0 +1,11 @@
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.setAttribute('data-cfasync', 'false');
+    script.src = '//amusinghump.com/2fff9116256672b819523a02a8cabcd7/invoke.js';
+    document.head.appendChild(script);
+
+    var container = document.createElement('div');
+    container.id = 'container-2fff9116256672b819523a02a8cabcd7';
+    document.body.appendChild(container);
+})();
