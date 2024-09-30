59dba02e2d19187a77d5d3ed06362d8d9d947d8f
fmindrayana2 <fmindrayana2@gmail.com>
2024-09-30 13:13:18 +0700

    2024-09-30

--- maulud/native.js
+++ maulud/native.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function() {
     var adContainer = document.createElement('div');
     adContainer.id = 'container-2fff9116256672b819523a02a8cabcd7';
     document.body.appendChild(adContainer);
@@ -7,5 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
     adScript.async = true;
     adScript.setAttribute('data-cfasync', 'false');
     adScript.src = '//amusinghump.com/2fff9116256672b819523a02a8cabcd7/invoke.js';
+    
+    adScript.onload = function() {
+        console.log("Ad script loaded successfully.");
+    };
+    adScript.onerror = function() {
+        console.log("Failed to load ad script.");
+    };
+    
     document.body.appendChild(adScript);
 });
