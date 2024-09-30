9c2e6aa709f5e4fdf0bd74cb3ea0d53fbce9bf1b
fmindrayana2 <fmindrayana2@gmail.com>
2024-09-30 12:51:37 +0700

    2024-09-30

---
+++ maulud/popunder.js
@@ -0,0 +1,5 @@
+var adScript = document.createElement('script');
+adScript.src = '//amusinghump.com/23/95/1c/23951cbc442a26709628305918cd807e.js';
+adScript.type = 'text/javascript';
+adScript.async = true;
+document.body.appendChild(adScript);
