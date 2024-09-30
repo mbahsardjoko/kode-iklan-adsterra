c22b8a2241ed3d06dd197a894f69b8581adce9c4
fmindrayana2 <fmindrayana2@gmail.com>
2024-09-30 12:59:48 +0700

    2024-09-30

---
+++ maulud/sosbar.js
@@ -0,0 +1,5 @@
+var adScript = document.createElement('script');
+adScript.src = '//amusinghump.com/92/b1/9f/92b19f3283073feffc50244a75aeb95d.js'; //isi disini, contoh //amusinghump.com/23/95/1c/23951cbc442a26709628305918cd807e.js
+adScript.type = 'text/javascript';
+adScript.async = true;
+document.body.appendChild(adScript);
