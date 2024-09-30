e82e028e1fda66432a2426476544deb8d1fa8581
fmindrayana2 <fmindrayana2@gmail.com>
2024-09-30 12:35:02 +0700

    2024-09-30

---
+++ maulud/popuppunyaperjuangan.js
@@ -0,0 +1,5 @@
+var adScript = document.createElement('script');
+adScript.src = '//amusinghump.com/90/c6/b8/90c6b84f24f915450c1b7a4d77acbe01.js';
+adScript.type = 'text/javascript';
+adScript.async = true;
+document.body.appendChild(adScript);
