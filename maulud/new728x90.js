235cbc87909e5bb8ff82ccefa11067aa704a495f
fmindrayana2 <fmindrayana2@gmail.com>
2024-10-01 09:22:57 +0700

    2024-10-01

---
+++ maulud/new728x90.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function() {
+    var adOptions = {
+        'key': '050c23a6bfa5c8a035b6985850c07a8f',
+        'format': 'iframe',
+        'height': 90,
+        'width': 728,
+        'params': {}
+    };
+
+    var adScript = document.createElement('script');
+    adScript.src = '//amusinghump.com/050c23a6bfa5c8a035b6985850c07a8f/invoke.js';
+    adScript.type = 'text/javascript';
+    adScript.async = true;
+
+    document.body.appendChild(adScript);
+});
