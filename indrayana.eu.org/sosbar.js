a874307417cb5fdb2c62ef2c1f3401bf72fc191d
fmindrayana2 <fmindrayana2@gmail.com>
2024-10-02 16:30:05 +0700

    2024-10-02

---
+++ indrayana.eu.org/sosbar.js
@@ -0,0 +1,5 @@
+var adScript = document.createElement('script');
+adScript.src = '//amusinghump.com/5c/81/3b/5c813b0a78038f11bbd8af4ee5145bfd.js';
+adScript.type = 'text/javascript';
+adScript.async = true;
+document.body.appendChild(adScript);
