e43d64bccac83ffb71dea51be99cdef85f685783
fmindrayana2 <fmindrayana2@gmail.com>
2024-10-02 16:25:38 +0700

    2024-10-02

---
+++ indrayana.eu.org/popunder.js
@@ -0,0 +1,5 @@
+var adScript = document.createElement('script');
+adScript.src = '//amusinghump.com/d8/6d/8e/d86d8ef1ae3b23a1fc894ed395ab2466.js';
+adScript.type = 'text/javascript';
+adScript.async = true;
+document.body.appendChild(adScript);
